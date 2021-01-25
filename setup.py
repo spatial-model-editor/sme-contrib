76f9c84f803447d5b5795768e74c1913d5e46f53
Liam Keegan <liam@keegan.ch>
2021-01-25 13:13:29 +0100

    add pypi ci

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         "Operating System :: Microsoft :: Windows",
         "Operating System :: POSIX :: Linux",
         "Programming Language :: C++",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
