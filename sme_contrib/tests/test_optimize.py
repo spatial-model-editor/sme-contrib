d1238fdf2ad863c5decfa79ab788ff858f5f368b
Liam Keegan <liam@keegan.ch>
2021-01-27 10:33:50 +0100

    add rescale & abs_diff tests

--- sme_contrib/tests/test_optimize.py
+++ sme_contrib/tests/test_optimize.py
@@ -1,4 +1,4 @@
-from sme_contrib.optimize import hessian
+import sme_contrib.optimize as opt
 import numdifftools
 import numpy as np
 
@@ -18,8 +18,24 @@ def test_hessian() -> None:
     h0 = numdifftools.Hessian(r)(r0)
     norm = np.sqrt(np.mean(np.power(h0, 2)))
     # compare to hessian() with default step size:
-    h1 = hessian(r, r0)
+    h1 = opt.hessian(r, r0)
     assert np.sqrt(np.mean(np.power(h1 - h0, 2))) / norm < 1e-4
     # compare to hessian() with smaller step size:
-    h2 = hessian(r, r0, 1e-5)
+    h2 = opt.hessian(r, r0, 1e-5)
     assert np.sqrt(np.mean(np.power(h2 - h0, 2))) / norm < 1e-6
+
+
+def test_rescale() -> None:
+    a = np.array([1.0, 3.0, 5.0])
+    a_rescaled = opt.rescale(a, 3.4)
+    factor = 3.4 / 5.0
+    assert len(a_rescaled) == 3
+    assert np.abs(a_rescaled[0] - factor) < 1e-13
+    assert np.abs(a_rescaled[1] - 3.0 * factor) < 1e-13
+    assert np.abs(a_rescaled[2] - 5.0 * factor) < 1e-13
+
+
+def test_abs_diff() -> None:
+    a = np.array([1.0, 3.0, 5.0])
+    b = np.array([2.0, 3.1, 5.0])
+    assert np.abs(opt.abs_diff(a, b) - 0.505) < 1e-13
