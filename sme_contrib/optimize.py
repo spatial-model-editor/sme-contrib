9fff9997d95b917ef1803a465cee28790cc9fd8a
Liam Keegan <liam@keegan.ch>
2021-01-27 10:28:28 +0100

    add rescale and abs_diff functions

--- sme_contrib/optimize.py
+++ sme_contrib/optimize.py
@@ -2,7 +2,7 @@ import numpy as np
 import multiprocessing
 
 
-def hessian_f(dat):
+def _hessian_f(dat):
     (typ, i, j, f, x0, rel_eps) = dat
     if typ == 0:
         x = np.array(x0, dtype=np.float64)
@@ -42,7 +42,7 @@ def hessian(f, x0, rel_eps=1e-2):
             dat.append((2, i, j, f, x0, -rel_eps))  # f_(i-,j-)
             idxs[(i, j)] = k
             k = k + 1
-    ff = pool.map(hessian_f, dat)
+    ff = pool.map(_hessian_f, dat)
     pool.close()
     pool.join()
     h = np.zeros((n, n))
@@ -67,3 +67,14 @@ def hessian(f, x0, rel_eps=1e-2):
             h[(i, j)] /= 2.0 * rel_eps * rel_eps * x0[i] * x0[j]
             h[(j, i)] = h[(i, j)]
     return h
+
+
+# rescale `x` such that the maximum element equals `new_max_element`
+def rescale(x, new_max_element):
+    old_max_element = np.amax(x)
+    return np.multiply(x, new_max_element / old_max_element)
+
+
+# sum of squares difference between every element of x and y
+def abs_diff(x, y):
+    return 0.5 * np.sum(np.power(x - y, 2))
